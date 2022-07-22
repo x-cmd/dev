0f70651cf3f0798f448a94cc346d700b4eaf6fd8
lijunhao <l@x-cmd.com>
2022-07-22 20:56:37 +0800

    ...

--- mod/awk/lib/j2y.sh
+++ mod/awk/lib/j2y.sh
@@ -1,5 +1,30 @@
 
+f1(){
 command awk -f default.awk -f json.awk -f jqparse.awk -f j2y.awk -f j2y.test.awk <<A
+{
+    "a": 3,
+    "b": 4,
+    "c": 6,
+    "d": [ 1, 2, "jhi", {
+        "a": 1,
+        "b": 2
+    } ],
+    "e": { "a": 1 }
+    "f": [
+        [1, 2, 3]
+    ]
+}
+{
+    "a": 1
+}
+[
+    [1, 2, 3]
+]
+A
+}
+
+
+command awk -f default.awk -f json.awk -f yml.awk -f ji2y.awk -f ji2y.test.awk <<A
 {
     "a": 3,
     "b": 4,
@@ -13,4 +38,7 @@ command awk -f default.awk -f json.awk -f jqparse.awk -f j2y.awk -f j2y.test.awk
 {
     "a": 1
 }
+[
+    [1, 2, 3]
+]
 A
